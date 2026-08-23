818e27e3a7e9349058c1041c959d3baeaed3b37f
karol <przadka93@gmail.com>
2026-08-23 19:28:00 +0200

    service skeleton

---
+++ src/main/java/com/kodano/inbox/InboxApplication.java
@@ -0,0 +1,12 @@
+package com.kodano.inbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InboxApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(InboxApplication.class, args);
+   }
+}
